4a0ddaf2042a6fbef5bbcd89a973c6a9af63f5f7
Chico <16483314@qq.com>
2018-10-06 19:01:36 +0800

    added aniguass

---
+++ src/anigauss.h
@@ -0,0 +1,6 @@
+#ifndef __ANIGAUSS_H
+#define __ANIGAUSS_H
+
+#include <stdio.h>
+
+#endif /* __ANIGAUSS_H */
---
+++ src/aniguass_mex.h
@@ -0,0 +1,6 @@
+#ifndef __ANIGAUSS_MEX_H
+#define __ANIGAUSS_MEX_H
+
+#include <stdio.h>
+
+#endif /* __ANIGAUSS_MEX_H */
